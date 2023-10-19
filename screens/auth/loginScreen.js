2401a9583118432c1aa7b2bf1f2b9ae0e944cd00
Nathan-source-smile <137820589+Nathan-source-smile@users.noreply.github.com>
2023-10-19 00:57:41 -0400

    firebase init

--- screens/auth/loginScreen.js
+++ screens/auth/loginScreen.js
@@ -1,3 +1,5 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable prettier/prettier */
 import {
   StyleSheet,
   Text,
@@ -8,20 +10,20 @@ import {
   BackHandler,
   Platform,
 } from 'react-native';
-import React, {useState, useCallback} from 'react';
-import {Colors, Fonts, Sizes, screenHeight} from '../../constants/styles';
+import React, { useState, useCallback } from 'react';
+import { Colors, Fonts, Sizes, screenHeight } from '../../constants/styles';
 import IntlPhoneInput from 'react-native-intl-phone-input';
-import {useFocusEffect} from '@react-navigation/native';
+import { useFocusEffect } from '@react-navigation/native';
 import MyStatusBar from '../../components/myStatusBar';
 
-const LoginScreen = ({navigation}) => {
+const LoginScreen = ({ navigation }) => {
   const backAction = () => {
     if (Platform.OS === 'ios') {
-      navigation.addListener('beforeRemove', e => {
+      navigation.addListener('beforeRemove', (e) => {
         e.preventDefault();
       });
     } else {
-      backClickCount == 1 ? BackHandler.exitApp() : _spring();
+      backClickCount === 1 ? BackHandler.exitApp() : _spring();
       return true;
     }
   };
@@ -34,7 +36,7 @@ const LoginScreen = ({navigation}) => {
         BackHandler.removeEventListener('hardwareBackPress', backAction);
         navigation.removeListener('gestureEnd', backAction);
       };
-    }, [backAction]),
+    }, [backAction])
   );
 
   function _spring() {
@@ -46,15 +48,18 @@ const LoginScreen = ({navigation}) => {
 
   const [backClickCount, setBackClickCount] = useState(0);
   const [phoneNumber, setPhoneNumber] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   return (
-    <View style={{flex: 1, backgroundColor: Colors.whiteColor}}>
+    <View style={{ flex: 1, backgroundColor: Colors.whiteColor }}>
       <MyStatusBar />
-      <View style={{flex: 1, justifyContent: 'flex-end'}}>
+      <View style={{ flex: 1, justifyContent: 'flex-end' }}>
         <ScrollView
           automaticallyAdjustKeyboardInsets={true}
           showsVerticalScrollIndicator={false}
-          contentContainerStyle={{justifyContent: 'flex-end', flexGrow: 1}}>
+          contentContainerStyle={{ justifyContent: 'flex-end', flexGrow: 1 }}
+        >
           {loginImage()}
           {welcomeInfo()}
           {mobileNumberInfo()}
@@ -72,8 +77,9 @@ const LoginScreen = ({navigation}) => {
         onPress={() => {
           navigation.push('Register');
         }}
-        style={styles.buttonStyle}>
-        <Text style={{...Fonts.whiteColor18Bold}}>Continue</Text>
+        style={styles.buttonStyle}
+      >
+        <Text style={{ ...Fonts.whiteColor18Bold }}>Continue</Text>
       </TouchableOpacity>
     );
   }
@@ -81,7 +87,7 @@ const LoginScreen = ({navigation}) => {
   function exitInfo() {
     return backClickCount == 1 ? (
       <View style={styles.exitInfoWrapStyle}>
-        <Text style={{...Fonts.whiteColor15SemiBold}}>
+        <Text style={{ ...Fonts.whiteColor15SemiBold }}>
           Press Back Once Again to Exit
         </Text>
       </View>
@@ -94,20 +100,46 @@ const LoginScreen = ({navigation}) => {
         style={{
           marginHorizontal: Sizes.fixPadding * 2.0,
           marginBottom: Sizes.fixPadding,
-        }}>
+        }}
+      >
         <IntlPhoneInput
-          onChangeText={({phoneNumber}) => setPhoneNumber(phoneNumber)}
+          onChangeText={({ phoneNumber }) => setPhoneNumber(phoneNumber)}
           defaultCountry="IN"
-          containerStyle={{backgroundColor: Colors.whiteColor}}
+          containerStyle={{ backgroundColor: Colors.whiteColor }}
           placeholder={'Enter Your Number'}
           phoneInputStyle={styles.phoneInputStyle}
           dialCodeTextStyle={{
             ...Fonts.blackColor15Bold,
             marginHorizontal: Sizes.fixPadding - 2.0,
           }}
-          modalCountryItemCountryNameStyle={{...Fonts.blackColor16Bold}}
-          flagStyle={{width:40.0,height:40.0,marginBottom:10.0,}}
-        />        
+          modalCountryItemCountryNameStyle={{ ...Fonts.blackColor16Bold }}
+          flagStyle={{ width: 40.0, height: 40.0, marginBottom: 10.0 }}
+        />
+      </View>
+    );
+  }
+
+  function emailInfo() {
+    return (
+      <View
+        style={{
+          marginHorizontal: Sizes.fixPadding * 2.0,
+          marginBottom: Sizes.fixPadding,
+        }}
+      >
+        <IntlPhoneInput
+          onChangeText={({ email }) => set}
+          defaultCountry="IN"
+          containerStyle={{ backgroundColor: Colors.whiteColor }}
+          placeholder={'Enter Your Number'}
+          phoneInputStyle={styles.phoneInputStyle}
+          dialCodeTextStyle={{
+            ...Fonts.blackColor15Bold,
+            marginHorizontal: Sizes.fixPadding - 2.0,
+          }}
+          modalCountryItemCountryNameStyle={{ ...Fonts.blackColor16Bold }}
+          flagStyle={{ width: 40.0, height: 40.0, marginBottom: 10.0 }}
+        />
       </View>
     );
   }
@@ -119,10 +151,12 @@ const LoginScreen = ({navigation}) => {
           marginHorizontal: Sizes.fixPadding * 2.0,
           marginTop: Sizes.fixPadding * 4.0,
           marginBottom: Sizes.fixPadding * 2.0,
-        }}>
-        <Text style={{...Fonts.blackColor20Bold}}>Welcome to Cabwind</Text>
+        }}
+      >
+        <Text style={{ ...Fonts.blackColor20Bold }}>Welcome to Cabwind</Text>
         <Text
-          style={{marginTop: Sizes.fixPadding, ...Fonts.grayColor14SemiBold}}>
+          style={{ marginTop: Sizes.fixPadding, ...Fonts.grayColor14SemiBold }}
+        >
           Enter your phone number to continue
         </Text>
       </View>
@@ -151,7 +185,7 @@ const styles = StyleSheet.create({
     ...Fonts.blackColor15Bold,
     borderBottomColor: Colors.shadowColor,
     borderBottomWidth: 1.0,
-    padding:0
+    padding: 0,
   },
   exitInfoWrapStyle: {
     backgroundColor: Colors.lightBlackColor,
