1bc07c2c8d9a108cb1a20c851fb20c802f95f134
Dragon-gold0808 <142170946+Dragon-gold0808@users.noreply.github.com>
2023-10-23 02:50:36 -0400

    10230250

--- src/screens/rating/ratingScreen.js
+++ src/screens/rating/ratingScreen.js
@@ -49,7 +49,10 @@ const RatingScreen = ({navigation}) => {
     return (
       <Text
         onPress={() => {
-          navigation.push('Home');
+          navigation.reset({
+            index: 0,
+            routes: [{name: 'Home'}],
+          });
         }}
         style={styles.backToHomeTextStyle}>
         Back to Home
